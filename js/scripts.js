dfb468607e0470fa32d09d596c77d3e1bd644f7c
utkarsh-naman <114791877+utkarsh-naman@users.noreply.github.com>
2022-12-02 20:22:02 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
 alert("Hi Rashmi, gotta say you something.");
 
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
 function togglegift(){
 var image = document.getElementById("gift");
 if (image.src.match("closed")){
 image.src="img/open.png";
+sleep(2000);
 window.open("https://youtube.com/shorts/0FSeplvUVSs?feature=share");}
 else{
 image.src="img/closed.png";}
