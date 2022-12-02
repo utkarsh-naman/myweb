93bc9372ef22c6b3733f3663efd1e049d0dea469
utkarsh-naman <114791877+utkarsh-naman@users.noreply.github.com>
2022-12-02 19:59:59 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 alert("Hi Rashmi, gotta say you something.");
+
+function togglegift(){
+var Image = document.getElementById("gift");
+if (Image.src.match("closed")){
+Image.src="img/open.png";
+window.open("https://youtube.com/shorts/0FSeplvUVSs?feature=share");}
+else{
+Image.src="img/closed.png";}
+}
