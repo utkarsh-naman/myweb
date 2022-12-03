ebaf5c5fd38bfb644ebac3a1ee14fc91f246d604
utkarsh-naman <114791877+utkarsh-naman@users.noreply.github.com>
2022-12-03 13:20:25 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -35,7 +35,7 @@ particlesJS('particles-js',
           "nb_sides": 5
         },
         "image": {
-          "src": "img/github.svg",
+          "src": "img/bluecap.png",
           "width": 100,
           "height": 100
         }
