a56f4730a700230d6f1f636ff905af746a54f1fb
Utkarsh Naman <114791877+utkarsh-naman@users.noreply.github.com>
2023-01-23 15:06:04 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 function openvid(){
-sleep(3000).then(()=> {window.open("https://youtube.com/shorts/0FSeplvUVSs?feature=share");});
+sleep(3000).then(()=> {window.open("https://youtu.be/qn0RSYOnlXU");});
 }
 
 function togglegift(){
