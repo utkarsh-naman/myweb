d25044b7415363ef89bdff9852e717f8e671b0da
Utkarsh Naman <114791877+utkarsh-naman@users.noreply.github.com>
2024-01-10 23:49:29 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-alert("Hi Harsh & Rohit, there's something for you");
+alert("Hi Kripa, there's something for you");
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
